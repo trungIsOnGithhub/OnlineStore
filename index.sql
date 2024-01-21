6f504d8a764d4246f9d7901d5203ce1b9e32d71a
trungvuive <trung150502@gmail.com>
2024-01-21 11:40:12 +0700

    create more index

--- index.sql
+++ index.sql
@@ -8,3 +8,12 @@
 
 CREATE INDEX product_by_supplier_id_idx
 ON product (supplier_id);
+
+CREATE INDEX product_by_price_idx
+ON product (price);
+
+CREATE UNIQUE INDEX employees_by_phone_number
+ON employees (phone_number);
+
+CREATE UNIQUE INDEX store_users_by_phone_number
+ON store_users (phone_number);
